b34ccca19428597c1ca6a403601cfc91046d6366
sanjaraT <sanjaratunola@gmail.com>
2026-01-13 13:04:13 +0600

    Preprocessing1

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import numpy as np 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("titanic_train.csv")
+# print(df.head())
+# print(df.shape)
+
+df['Age'].fillna(df['Age'].median(),inplace=True)
+df.drop(columns=["PassengerId","Name","Cabin","Ticket",],inplace=True)
+df['Embarked'].fillna(df['Embarked'].mode()[0],inplace=True)
+df['Sex'] = df['Sex'].map({'male':0, 'female':1})
+df = pd.get_dummies(df, columns=['Embarked'],drop_first=True)
 print(df.head())
+
+#Correlation
+corr_matrix = df.corr()
+
+plt.figure(figsize=(12,10))
+sns.heatmap(
+    corr_matrix, annot=True,
+    fmt = ".2f", cmap="coolwarm"
+)
+plt.title("Correlation Matrix")
+plt.show()
+
+
+# print(df.isnull().sum())
+
+
 
