31e04caeb9fc988b9976fbc160ac024cbe7927b6
sanjaraT <sanjaratunola@gmail.com>
2026-01-14 16:41:46 +0600

    Logistic Regression Training

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
 
 df = pd.read_csv("titanic_train.csv")
 df_test = pd.read_csv("titanic_test.csv")
+
 # print(df.head())
 # print(df.shape)
+# print(df.isnull().sum())
 
 df['Age'].fillna(df['Age'].median(),inplace=True)
 df.drop(columns=["PassengerId","Name","Cabin","Ticket",],inplace=True)
@@ -44,25 +50,47 @@ X_val_scaled = scaler.transform(X_val)
 X_test_scaled = scaler.transform(df_test)
 
 #Correlation
-scaled_df = pd.DataFrame(X_train_scaled, columns=X_train.columns)
-corr_matrix = scaled_df.corr()
-plt.figure(figsize=(12,10))
-sns.heatmap(
-    corr_matrix, annot=True,
-    fmt = ".2f", cmap="coolwarm"
-)
-plt.title("Correlation Matrix")
-plt.show()
+# scaled_df = pd.DataFrame(X_train_scaled, columns=X_train.columns)
+# corr_matrix = scaled_df.corr()
+# plt.figure(figsize=(12,10))
+# sns.heatmap(
+#     corr_matrix, annot=True,
+#     fmt = ".2f", cmap="coolwarm"
+# )
+# plt.title("Correlation Matrix")
+# plt.show()
 
 # Class Distribution
-plt.figure(figsize=(5,4))
-df["Survived"].value_counts().plot(kind = "bar",)
-plt.xticks([0,1],["Yes","No"],rotation = 0)
-plt.ylabel("Count")
-plt.title("Distribution")
-plt.show()
+# plt.figure(figsize=(5,4))
+# df["Survived"].value_counts().plot(kind = "bar",)
+# plt.xticks([0,1],["Yes","No"],rotation = 0)
+# plt.ylabel("Count")
+# plt.title("Distribution")
+# plt.show()
 
-# print(df.isnull().sum())
+#LOGISTIC REGRESSION
+lr = LogisticRegression(
+    max_iter=1000,
+    random_state=42
+)
+param_grid = {
+    'C':[0.01,0.1,1,10],
+    'penalty':['l2'],
+    'solver':['liblinear','lbfgs']
+}
+grid_search = GridSearchCV(
+    estimator=lr,
+    param_grid=param_grid,
+    cv = 5,
+    scoring = 'f1',
+    n_jobs = -1
+)
 
+grid_search.fit(X_train_scaled,y_train)
+best_lr = grid_search.best_estimator_
 
+#prediction
+y_val_pred = best_lr.predict(X_val_scaled)
+val_acc = accuracy_score(y_val, y_val_pred)
+print(classification_report(y_val,y_val_pred))
 
