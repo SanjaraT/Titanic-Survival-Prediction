299a2f9aef340a4bc7c3701b1bc07f742f10aae8
sanjaraT <sanjaratunola@gmail.com>
2026-01-16 12:03:00 +0600

    Graddient Boosting

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 
 df = pd.read_csv("titanic_train.csv")
@@ -104,35 +105,68 @@ X_test_scaled = scaler.transform(df_test)
 # plt.show()
 
 #RANDOM FOREST
-rf = RandomForestClassifier(
-    random_state=42,
-    n_jobs=-1
+# rf = RandomForestClassifier(
+#     random_state=42,
+#     n_jobs=-1
+# )
+# param_grid={
+#     'n_estimators':[100,200,300],
+#     'max_depth':[None,5,10],
+#     'min_samples_split':[2,5],
+#     'min_samples_leaf':[1,2]
+# }
+# grid_rf = GridSearchCV(
+#     estimator=rf,
+#     param_grid=param_grid,
+#     cv = 5,
+#     scoring = 'f1',
+#     n_jobs = -1
+# )
+# grid_rf.fit(X_train_scaled,y_train)
+# best_rf = grid_rf.best_estimator_
+
+# #prediction
+# y_val_pred = best_rf.predict(X_val_scaled)
+# val_acc = accuracy_score(y_val, y_val_pred)
+# print(classification_report(y_val,y_val_pred))
+
+# #confusison_matrix
+# cm_rf = confusion_matrix(y_val,y_val_pred)
+# plt.figure(figsize=(5,4))
+# sns.heatmap(cm_rf,annot=True, fmt ='d',cmap='Blues')
+# plt.xlabel("Predicted")
+# plt.ylabel("Actual")
+# plt.show()
+
+#GRADIENT BOOSTING
+gb = GradientBoostingClassifier(
+    random_state=42
 )
-param_grid={
-    'n_estimators':[100,200,300],
-    'max_depth':[None,5,10],
-    'min_samples_split':[2,5],
-    'min_samples_leaf':[1,2]
+param_grid ={
+    'n_estimators':[100,200],
+    'learning_rate':[0.05,0.1],
+    'max_depth':[3,5],
+    'subsample':[0.8,1.0]
 }
-grid_rf = GridSearchCV(
-    estimator=rf,
+grid_gb = GridSearchCV(
+    estimator=gb,
     param_grid=param_grid,
     cv = 5,
     scoring = 'f1',
     n_jobs = -1
 )
-grid_rf.fit(X_train_scaled,y_train)
-best_rf = grid_rf.best_estimator_
+grid_gb.fit(X_train_scaled,y_train)
+best_gb = grid_gb.best_estimator_
 
 #prediction
-y_val_pred = best_rf.predict(X_val_scaled)
-val_acc = accuracy_score(y_val, y_val_pred)
-print(classification_report(y_val,y_val_pred))
+y_val_pred_gb = best_gb.predict(X_val_scaled)
+val_acc = accuracy_score(y_val, y_val_pred_gb)
+print(classification_report(y_val,y_val_pred_gb))
 
 #confusison_matrix
-cm_rf = confusion_matrix(y_val,y_val_pred)
+cm_gb = confusion_matrix(y_val,y_val_pred_gb)
 plt.figure(figsize=(5,4))
-sns.heatmap(cm_rf,annot=True, fmt ='d',cmap='Blues')
+sns.heatmap(cm_gb,annot=True, fmt ='d',cmap='Blues')
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.show()
